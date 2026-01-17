651ce4cc73defc285d7f54a69ade2c4134451d96
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2026-01-17 23:08:03 +0600

    update read-file action.

--- src/lib/actions/read-file.ts
+++ src/lib/actions/read-file.ts
@@ -4,12 +4,24 @@ import { file } from "bun";
 
 export default async function readFile() {
   try {
-    const text = await file("./src/app/globals.css").text();
-    return {
-      error: false,
-      data: text,
-      message: "Text data",
-    };
+    const readFile = file("./src/app/globals.css");
+
+    return await readFile
+      .text()
+      .then((text) => {
+        return {
+          error: false,
+          data: text,
+          message: "Text data",
+        };
+      })
+      .catch(() => {
+        return {
+          error: true,
+          data: null,
+          message: "Failed to read file.",
+        };
+      });
   } catch {
     return {
       error: true,
